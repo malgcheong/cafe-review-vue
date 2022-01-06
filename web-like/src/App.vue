8952cbbba06c4c0af0719dc4c7dbc754e63beb17
Yeojinny <pdg03092@gmail.com>
2022-01-06 20:21:48 +0900

    두번째 커밋

---
+++ web-like/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <like-header></like-header>
+    <like-list></like-list>
+    <like-footer></like-footer>
+  </div>
+</template>
+
+<script>
+import LikeHeader from './components/LikeHeader.vue'
+import LikeList from './components/LikeList.vue'
+import LikeFooter from './components/LikeFooter.vue'
+
+export default {
+  data(){
+    return {
+
+    }
+  },
+  components:{
+    'like-header':LikeHeader,
+    'like-list':LikeList,
+    'like-footer':LikeFooter
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ web-like/src/components/LikeFooter.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <button @click="deleteList">삭제</button>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
