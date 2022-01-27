f2b30e4543ac38a9da764bec6e95993b860f05fb
malgcheong <skacjddn7@gmail.com>
2022-01-27 09:26:27 +0900

    feature: merge join page
        이용약관, 로그인폼, footer 합치기

---
+++ cafe-community/src/components/join/JoinForm.vue
@@ -0,0 +1,82 @@
+<template>
+    <form>
+        <div>
+            <label for="name">이름</label>:
+            <input type="text" name="name" id="name">
+        </div>
+        <div>
+            <label for="email">이메일</label>:
+            <input type="text" id="email">
+            <button type="button" v-on:click="getPasscode">인증 요청</button>
+        </div>
+        <div :class= "{getDisplay}">
+            <label for="passcode">인증번호</label>:
+            <input type="text" id="passcode" placeholder="인증번호 입력">
+            <button type="button" v-on:click="verifyPasscode">확인</button>
+        </div>
+        <div>
+            <label for="password">비밀번호</label>:
+            <input type="text" id="password">
+        </div>
+        <div>
+            <label for="password_check">비밀번호 확인</label>:
+            <input type="text" id="password_check">
+        </div>
+    </form>
+</template>
+
+<script>
+    import axios from 'axios';
+    export default {
+        data: function(){
+            return{
+                display: "item_display_none",
+                readonly: false,
+                users:{}
+            }
+        },
+        methods:{
+            getPasscode:function(){
+                // 인증번호 요청
+                axios.post('join/aut')
+                .then(function(response) {
+                    console.log(response)
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+
+                //인증번호 display
+                this.display = "item_display";
+
+                //이메일 readonly
+                this.readonly = true;
+            },
+            verifyPasscode:function(){
+                // 인증번호 확인
+                axios.post('join/aut/verify')
+                .then(function(response) {
+                    console.log(response)
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+
+            },
+        },
+        computed:{
+            getDisplay:function(){
+                return this.display;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .item_display{
+        display: block;
+    }
+    .item_display_none{
+        display: none;
+    }
+</style>
