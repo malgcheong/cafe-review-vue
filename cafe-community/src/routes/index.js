96f4c823e311f52682de9048b2c54132019f3e9a
HyojeongRyu <ryu8701@gmail.com>
2022-03-10 20:38:35 +0900

    Feat: 라우터 추가중
    회원가입 완료 후 모달창 확인 버튼을 누르면 home으로 이동하기 위해
    라우터 추가중

---
+++ cafe-community/src/routes/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../components/home.vue';
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+    mode:'history',
+    routes:[
+        {
+            path:'/',
+            component:Home
+        }
+    ]
+})
