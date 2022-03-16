8c8a18fb198f5c436c358d6269d4448e9b716744
malgcheong <skacjddn7@gmail.com>
2022-03-17 01:40:59 +0900

    홈페이지 작성중-2

--- cafe-community/src/components/home/PostList.vue
+++ cafe-community/src/components/home/PostList.vue
@@ -2,29 +2,36 @@
 
 <template>
 <div>
-    <section v-for="(items,key) in cafeList" :key="key" class="recent">
-        <div>
+    <section v-for="(items,key) in cafeList" :key="key">
+        <div class="cafe_title">
             {{items.title}}
         </div>
-        <div v-for="(item,key) in items" :key="key">
-            <div><img :src= "images + item.image" alt=""></div>
+        <div class="cafe_image">
+            <div v-for="(item,key) in items.List" :key="key">
+                <img :src= "getImage(item.image)" alt="" style="width:200px; height:200px;">
+            </div>
         </div>
     </section>
-    <div><img :src= a alt=""></div>
 
 </div>
 </template>
 <script>
     import axios from 'axios';
-    import images from '@/assets/images/';
-
     export default {
         data(){
             return{
                 cafeList:{},
-                images:images
+                images: '../../assets/images/'
             }
         },
+        methods:{
+            getImage(image){
+                var imgContext = require.context('../../assets/images/', true, /\.*$/);
+                return imgContext('./'+image);
+            }
+            
+        },
+
         created(){
             axios.get('http://localhost:3000').then((Response)=>{
                 this.cafeList=Response.data.cafeList;
@@ -38,5 +45,11 @@
 </script>
 
 <style scoped>
-
+    .cafe_image{
+        display: flex;
+        justify-content: space-around;
+    }
+    .cafe_title{
+        margin:100px 0px 10px 250px;
+    }
 </style>
